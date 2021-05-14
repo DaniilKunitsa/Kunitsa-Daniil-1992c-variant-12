cfd19a25e48400dd134114c0e32df4e37c187033
Даниил Куница <kunitsadaniil@gmail.com>
2021-05-14 23:46:19 +0700

    Добавлен класс для вычислительных методов

---
+++ MainMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Куница_Даниил_1992с_Вариант_12
+{
+    class MainMethods
+    {
+    }
+}
