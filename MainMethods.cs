7350a0e488ca3d1d47b2c8b7538753805eea71ce
Даниил Куница <kunitsadaniil@gmail.com>
2021-05-14 23:48:33 +0700

    Добавлен метод для вывода двумерного массива на экран

--- MainMethods.cs
+++ MainMethods.cs
@@ -20,5 +20,16 @@ namespace Куница_Даниил_1992с_Вариант_12
                 }
             }
         }
+        public static void WriteArray(double[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
