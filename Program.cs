abb0e0d13aee52cc699333df2917a539c48b0238
Даниил Куница <kunitsadaniil@gmail.com>
2021-05-14 23:51:12 +0700

    Добавлен функционал, который демонстрирует работу поиска максимального числа среди элементов лежащих главной диагонали

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace Куница_Даниил_1992с_Вариант_12
     {
         static void Main(string[] args)
         {
+            double[,] array = new double[1000, 1000];
+            MainMethods.FillArray(array);
+            MainMethods.WriteArray(array);
+            Console.WriteLine($"\nМаксимальный элемент выше главной диагонали: {MainMethods.FindMax(array)}");
+            Console.ReadKey();
         }
     }
 }
